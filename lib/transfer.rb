760679f068c8a8172f597aeff297e906bc386e4d
Kierra M <k.merriman32@hotmail.com>
2019-09-27 16:43:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ attr_accessor :sender, :amount, :receiver, :status
   end
 
   def execute_transaction 
-      if  !valid?  # this is saying to check if not valid do this else..other code
+      if  !valid?  # this is saying to check if not valid and do this else..other code
        @status = "rejected"
        return "Transaction rejected. Please check your account balance."
      
